5a482eda561ebdac8261a155daa65744cfcdbea3
PushkarPisolkar04 <pushkarppisolkar@gmail.com>
2026-01-04 19:18:18 +0530

    Update

---
+++ config.example.py
@@ -0,0 +1,6 @@
+# config.example.py
+# Rename this file to config.py and add your API keys inside.
+
+# VirusTotal API Key (Get it from https://www.virustotal.com/gui/user/apikey)
+# Leave empty if you don't want to use VirusTotal checks.
+VIRUSTOTAL_API_KEY = ""
